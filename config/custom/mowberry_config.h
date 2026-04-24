dc1d09bebb3aaf1b4040016d1c4db8b4e93ad9b0
Paul Bouchier <paul.bouchier@gmail.com>
2026-04-24 15:01:47 -0500

    Mowberry support
    - add mowberry config , mowbot wheels spin
    - Support quadrature encoders on Mowberry
    - update battery voltage calc
    - calibrate odom rotation

--- config/custom/mowberry_config.h
+++ config/custom/mowberry_config.h
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef ESP32_CONFIG_H
-#define ESP32_CONFIG_H
+#ifndef MOWBERRY_CONFIG_H
+#define MOWBERRY_CONFIG_H
 
-#define LED_PIN LED_BUILTIN //used for debugging status
+#define LED_PIN 2 //used for debugging status
 
 //uncomment the base you're building
 #define LINO_BASE DIFFERENTIAL_DRIVE       // 2WD and Tracked robot w/ 2 motors
@@ -25,12 +25,12 @@
 //uncomment the motor driver you're using
 #define USE_GENERIC_2_IN_MOTOR_DRIVER      // Motor drivers with 2 Direction Pins(INA, INB) and 1 PWM(ENABLE) pin ie. L298, L293, VNH5019
 // #define USE_GENERIC_1_IN_MOTOR_DRIVER   // Motor drivers with 1 Direction Pin(INA) and 1 PWM(ENABLE) pin.
-// #define USE_BTS7960_MOTOR_DRIVER        // BTS7970 Motor Driver using A4950 (<40V) module or DRV8833 (<10V)
+// #define USE_BTS7960_MOTOR_DRIVER        // BTS7970 Motor Driver
 // #define USE_ESC_MOTOR_DRIVER            // Motor ESC for brushless motors
 
 //uncomment the IMU you're using
 // #define USE_GY85_IMU
-// #define USE_BNO085_IMU
+#define USE_BNO085_IMU
 // #define USE_MPU6050_IMU
 // #define USE_MPU9150_IMU
 // #define USE_MPU9250_IMU
@@ -64,62 +64,63 @@ ROBOT ORIENTATION
 */
 
 //define your robot' specs here
-#define MOTOR_MAX_RPM 150                   // motor's max RPM
+#define MOTOR_MAX_RPM 65                    // motor's max RPM
 #define MAX_RPM_RATIO 0.85                  // max RPM allowed for each MAX_RPM_ALLOWED = MOTOR_MAX_RPM * MAX_RPM_RATIO
-#define MOTOR_OPERATING_VOLTAGE 12          // motor's operating voltage (used to calculate max RPM)
-#define MOTOR_POWER_MAX_VOLTAGE 12          // max voltage of the motor's power source (used to calculate max RPM)
-#define MOTOR_POWER_MEASURED_VOLTAGE 12     // current voltage reading of the power connected to the motor (used for calibration)
-#define COUNTS_PER_REV1 450                 // wheel1 encoder's no of ticks per rev
-#define COUNTS_PER_REV2 450                 // wheel2 encoder's no of ticks per rev
+#define MOTOR_OPERATING_VOLTAGE 26          // motor's operating voltage (used to calculate max RPM)
+#define MOTOR_POWER_MAX_VOLTAGE 26          // max voltage of the motor's power source (used to calculate max RPM)
+#define MOTOR_POWER_MEASURED_VOLTAGE 26     // current voltage reading of the power connected to the motor (used for calibration)
+#define COUNTS_PER_REV1 2800                // wheel1 encoder's no of ticks per rev
+#define COUNTS_PER_REV2 2800                // wheel2 encoder's no of ticks per rev
 #define COUNTS_PER_REV3 450                 // wheel3 encoder's no of ticks per rev
 #define COUNTS_PER_REV4 450                 // wheel4 encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.0560               // wheel's diameter in meters
-#define LR_WHEELS_DISTANCE 0.224            // distance between left and right wheels
+#define WHEEL_DIAMETER 0.20                 // wheel's diameter in meters
+#define LR_WHEELS_DISTANCE 0.52             // distance between left and right wheels
 #define PWM_BITS 10                         // PWM Resolution of the microcontroller
 #define PWM_FREQUENCY 20000                 // PWM Frequency
+#define USE_SHORT_BRAKE                     // for shorter stopping distance
 
 // INVERT ENCODER COUNTS
-#define MOTOR1_ENCODER_INV false
+#define MOTOR1_ENCODER_INV true
 #define MOTOR2_ENCODER_INV false
 #define MOTOR3_ENCODER_INV false
 #define MOTOR4_ENCODER_INV false
 
 // INVERT MOTOR DIRECTIONS
-#define MOTOR1_INV false
+#define MOTOR1_INV true
 #define MOTOR2_INV false
 #define MOTOR3_INV false
 #define MOTOR4_INV false
 
 // ENCODER PINS
-#define MOTOR1_ENCODER_A 19
+#define MOTOR1_ENCODER_A 36
 #define MOTOR1_ENCODER_B 39
 
-#define MOTOR2_ENCODER_A 35
-#define MOTOR2_ENCODER_B 34
+#define MOTOR2_ENCODER_A 34
+#define MOTOR2_ENCODER_B 35
 
-#define MOTOR3_ENCODER_A 32
-#define MOTOR3_ENCODER_B 27
+#define MOTOR3_ENCODER_A -1
+#define MOTOR3_ENCODER_B -1
 
-#define MOTOR4_ENCODER_A 26
-#define MOTOR4_ENCODER_B 25
+#define MOTOR4_ENCODER_A -1
+#define MOTOR4_ENCODER_B -1
 
 // MOTOR PINS
 #ifdef USE_GENERIC_2_IN_MOTOR_DRIVER
-  #define MOTOR1_PWM 21
-  #define MOTOR1_IN_A 17
-  #define MOTOR1_IN_B 16
+  #define MOTOR1_PWM 23
+  #define MOTOR1_IN_A 19
+  #define MOTOR1_IN_B 18
 
-  #define MOTOR2_PWM 5
-  #define MOTOR2_IN_A 6
-  #define MOTOR2_IN_B 8
+  #define MOTOR2_PWM 27
+  #define MOTOR2_IN_A 25
+  #define MOTOR2_IN_B 26
 
-  #define MOTOR3_PWM 22
-  #define MOTOR3_IN_A 23
-  #define MOTOR3_IN_B 0
+  #define MOTOR3_PWM -1
+  #define MOTOR3_IN_A -1
+  #define MOTOR3_IN_B -1
 
-  #define MOTOR4_PWM 4
-  #define MOTOR4_IN_A 3
-  #define MOTOR4_IN_B 2
+  #define MOTOR4_PWM -1
+  #define MOTOR4_IN_A -1
+  #define MOTOR4_IN_B -1
 
   #define PWM_MAX pow(2, PWM_BITS) - 1
   #define PWM_MIN -PWM_MAX
@@ -142,7 +143,7 @@ ROBOT ORIENTATION
   #define MOTOR4_IN_A 3
   #define MOTOR4_IN_B -1 //DON'T TOUCH THIS! This is just a placeholder
 
-  #define PWM_MAX pow(2, PWM_BITS) - 1
+  #define PWM_MAX pow(2, PWM_BITS) - Right M-1
   #define PWM_MIN -PWM_MAX
 #endif
 
@@ -188,57 +189,44 @@ ROBOT ORIENTATION
   #define PWM_MIN -PWM_MAX
 #endif
 
-// Wifi network settings
-// Option 1: Enable WiFi by copying wifi_config.h.template in this directory
-// to wifi_config.h and filling in your wifi credentials and IP addresses.
-// wifi_config.h will be ignored by git, so your credentials will not be pushed to github,
-// and will be private to you. This is the recommended option.
-// Option 2: Enable WiFi below by filling in your wifi credentials and IP addresses.
-// WARNING: if you push this file to git, your credentials will be public!
+// Serial comms settings
+#define BAUDRATE 921600
+#define SDA_PIN 21 // specify I2C pins
+#define SCL_PIN 22
 
+// Wifi network settings
+// Enable WiFi by copying wifi_config.h.template to wifi_config.h and filling
+// in your wifi credentials, and IP addresses. This wifi_config.h will be ignored by git,
+// so your credentials will not be shared.
 #if __has_include("wifi_config.h")
   #include "wifi_config.h"
 #else
   // Enable WiFi with null terminated list of multiple APs SSID and password
-  // #define WIFI_AP_LIST {{"WIFI_SSID", "WIFI_PASSWORD"}, {NULL, NULL}}
-  #define AGENT_IP { 192, 168, 1, 100 }
+  // #define WIFI_AP_LIST {{"WIFI_SSID", "WIFI_PASSWORD"}, {NULL}}
+  #define AGENT_IP { 192, 168, 1, 100 }  // home wifi
   #define SYSLOG_SERVER { 192, 168, 1, 100 }
-  #define LIDAR_SERVER { 192, 168, 1, 100 }
+  #define LIDAR_SERVER { 192, 168, 1, 100 }  // eg IP of the desktop computer
 #endif
-
-// Uncomment the line below to enable Arduino OTA updates.
-// #define USE_ARDUINO_OTA
-// Uncomment the line below to enable syslog for debugging over wifi.
-// #define USE_SYSLOG
-#ifdef USE_WIFI
-  #ifndef USE_ARDUINO_OTA
-    #define USE_ARDUINO_OTA
-  #endif
-  #ifndef USE_SYSLOG
-    #define USE_SYSLOG
-  #endif
-#endif
-
-#define WIFI_MONITOR 2 // min. period to send wifi signal strength to syslog
 #define AGENT_PORT 8888
-#define SYSLOG_PORT 514
-#define DEVICE_HOSTNAME "esp32"
-#define APP_NAME "hardware"
+#define LIDAR_PORT 8889
+#define WIFI_MONITOR 2 // min. period to send wifi signal strength to syslog
+#define USE_ARDUINO_OTA
+
+// LIDAR settings
 // #define USE_LIDAR_UDP
-#define LIDAR_RXD 14
+#define LIDAR_RXD 4
 // #define LIDAR_PWM 15
 #define LIDAR_SERIAL 1 // uart number
 #define LIDAR_BAUDRATE 230400
-#define LIDAR_PORT 8889
-#define BAUDRATE 921600
-#define SDA_PIN 21 // specify I2C pins
-#define SCL_PIN 22
-#define NODE_NAME "esp32"
-// #define TOPIC_PREFIX "esp32/"
 
-// battery voltage ADC pin
-#define BATTERY_PIN 33
-// 3.3V ref, 12 bits ADC, 33k + 10k voltage divider
+#define NODE_NAME "mowberry"
+// #define TOPIC_PREFIX "mowberry/"
+
+// Battery settings
+// battery voltage ADC pin. If defined. battery voltage will be read from this pin. 
+#define BATTERY_PIN 32
+// If defined, battery voltage will be read from INA219 sensor.
+// #define USE_INA219
 // #define USE_ADC_LUT
 #ifdef USE_ADC_LUT
 const int16_t ADC_LUT[4096] = { /* insert adc_calibrate data here */ };
@@ -250,11 +238,13 @@ const int16_t ADC_LUT[4096] = { /* insert adc_calibrate data here */ };
 // ESP32 uses analogReadMilliVolts() which returns voltage in mV
 #define BATTERY_ADJUST(v) ((v) * ((10 + 1) / 1) / 1000.0)
 #endif
-// #define USE_INA219
-// #define TRIG_PIN 31 // ultrasonic sensor HC-SR04
-// #define ECHO_PIN 32
-#define USE_SHORT_BRAKE // for shorter stopping distance
+
+// Ultrasonic range sensor HC-SR04 pin settings
+// #define TRIG_PIN 13
+// #define ECHO_PIN 12
+
 // #define WDT_TIMEOUT 60 // Sec
+
 #define BOARD_INIT { \
     Wire.begin(SDA_PIN, SCL_PIN); \
     Wire.setClock(400000); \
@@ -262,6 +252,11 @@ const int16_t ADC_LUT[4096] = { /* insert adc_calibrate data here */ };
 // #define BOARD_INIT_LATE {}
 // #define BOARD_LOOP {}
 
+// Syslog settings
+#define USE_SYSLOG
+#define SYSLOG_PORT 514
+#define DEVICE_HOSTNAME "mowberry"
+#define APP_NAME "hardware"
 #ifdef USE_SYSLOG
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){ \
     syslog(LOG_ERR, "%s RCCHECK failed %d", __FUNCTION__, temp_rc); \
